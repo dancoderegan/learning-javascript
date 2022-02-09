2f0aca560581de088954eabf3ddeb6a063e85a40
dancoderegan <danielkoregan@gmail.com>
2022-02-09 18:16:23 -0330

    var to const test.js

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
-var myName = "dan";
-var myStr = "My name is " + myName + " and I am well!";
+const myName = "dan";
+const myStr = "My name is " + myName + " and I am well!";
 console.log(myStr)
 
-var firstLetterOfFirstName = "";
-var firstName = "Dan";
+const firstLetterOfFirstName = "";
+const firstName = "Dan";
 firstLetterOfFirstName = firstName[0];
 
 console.log(firstLetterOfFirstName);
 
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
-  var result ="The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
+  const result ="The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
   return result;
 }
 console.log(wordBlanks("dog", "big", "ran", "quickly"));
 console.log(wordBlanks("bike", "slow", "flew", "slowly"));
-var myArray = ["Dan", 31]
+const myArray = ["Dan", 31]
 console.log(myArray);
 
-var myList = [["cereal", 3], ["milk", 2], ["bananas", 1], ["eggs", 12]];
+const myList = [["cereal", 3], ["milk", 2], ["bananas", 1], ["eggs", 12]];
 
 console.log(myList);
 
